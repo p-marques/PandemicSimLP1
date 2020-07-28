a02c2aa0deb750c8d4c2fb02889a6ed51aa77e50
Pedro Dias Marques <pmark.pt@gmail.com>
2020-07-28 14:40:05 +0100

    Project setup

---
+++ PandemicSim/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PandemicSim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
