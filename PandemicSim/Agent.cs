8b428a1467dd3aa154364d55bd9ca91d4da7c8f2
AronSton265 <AronSton265@users.noreply.github.com>
2020-07-29 10:42:36 +0100

    add Agent

---
+++ PandemicSim/Agent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PandemicSim
+{
+    /// <summary>
+    /// this class represent the person on a tile and its states
+    /// <summary>
+    class Agent
+    {
+        /// <summary>
+        /// the tile where this agent is
+        /// <summary>
+        public Tile tile;
+
+        public bool isInfected { get; private set; }
+
+        public bool isDead { get; private set; }
+
+        /// <summary>
+        /// sets the tile of this agent to a new one
+        /// <summary>
+        public void setsetTile(Tile newTile) 
+        {
+            this.tile = newTile;
+        }
+
+    }
+}
