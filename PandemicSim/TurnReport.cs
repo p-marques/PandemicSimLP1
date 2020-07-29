2e233859775fb796bbe5a95ade513f520ad6175f
AronSton265 <AronSton265@users.noreply.github.com>
2020-07-29 12:02:14 +0100

    Add TurnReport

---
+++ PandemicSim/TurnReport.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PandemicSim
+{
+    /// </summary>
+    /// Stores the number of healthy, infected and dead agents
+    /// </summary>
+    class TurnReport
+    {
+        public int Healthy { get; }
+        public int Infected { get; }
+        public int Dead { get; }
+
+        public TurnReport(int healthyNumb, int infectedNumb, int deadNumb) 
+        {
+            this.Healthy = healthyNumb;
+            this.Infected = infectedNumb;
+            this.Dead = deadNumb;
+        }
+    }
+}
